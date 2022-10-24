63908fbcb240bb1050813a62799355e6d56b2f88
braverokmc79 <braverokmc79@gmail.com>
2022-10-24 22:35:31 +0900

    10강-NPM 패키지 (3rd party module)

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-console.log("Hello from Daniel");
-const add = require("./add");
-const mult = require("./mult");
-const stat = require("./stat");
 
-const result1 = add(1, 2);
-const result2 = mult(10, 2);
+const readline = require('readline-sync');
+const name = readline.question("what is your name?");
 
-console.log(result1, result2);
-console.log(stat.pi, stat.mean(1, 2, 3, 4, 5));
+console.log(name);
